2b5b0b967f162e188385b422234f114570f557f6
Patern14 <56110721+Patern14@users.noreply.github.com>
2022-02-03 11:43:14 +0000

    Api Key removed from Github.

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '56cd2bc854f7572d399bf1863debee07',
+    apiKey: 'YourApiKey',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
